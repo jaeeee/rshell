4d0d10a87191b9aeae5c46264b50f921ce2acc62
Daniel Kwong <dkwon014@ucr.edu>
2019-05-10 01:48:30 -0700

    unit test class, google unit testing submodule added.

---
+++ unit_test.cpp
@@ -0,0 +1,16 @@
+//test
+#include "base.h"
+
+#include "gtest/gtest.h"
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+
+int main(int argc, char **argv) {
+
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
