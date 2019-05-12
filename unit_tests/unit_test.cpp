c791ffe3002ca5812ca7d5a7999079282be95850
Jason Chang <jchan149@ucr.edu>
2019-05-11 23:49:11 -0700

    tests

--- unit_tests/unit_test.cpp
+++ unit_tests/unit_test.cpp
@@ -7,6 +7,8 @@
 #include "../header/and.h"
 #include "../header/pipe.h"
 #include "../header/semi.h"
+#include "../header/base.h"
+#include "../header/command.h"
 #include <iostream>
 #include <string>
 #include <cmath>
@@ -16,7 +18,7 @@ using namespace std;
 TEST(TestName, EchoTests) {
   string cmd = "a";
   Command *test = new Command(cmd);
-  EXPECT_FALSE(test->execute());
+  EXPECT_TRUE(test->execute());
   delete test;
 }
 
@@ -60,6 +62,55 @@ TEST(TestName, EchoTest6) {
   Command *test = new Command(cmd);
   EXPECT_TRUE(test->execute());
   delete test;
+}
+
+TEST(TestName, getCommandTest) {
+  string cmd = "a || ls";
+  Command *test = new Command(cmd);
+  EXPECT_TRUE(test->execute());
+  delete test;
+}
+TEST(TestName, getCommandTest2) {
+  string cmd = "ls && ls";
+  Command *test = new Command(cmd);
+  EXPECT_TRUE(test->execute());
+  delete test;
+}
+
+TEST(TestName, getCommandTest3) {
+  string cmd = "ls ; a";
+  Command *test = new Command(cmd);
+  EXPECT_TRUE(test->execute());
+  delete test;
+}
+
+TEST(TestName, CombinationTest1) {
+  string cmd1 = "ls #ls";
+  string cmd2 = "ls -a";
+  string cmd3 = "ls -l";
+
+  Base *test = new Command(cmd1);
+  Base *temp = new Command(cmd2);
+  Base *test2 = new And_Connector(test, temp);
+  EXPECT_TRUE(test2->execute());
+  //delete test;
+  //delete test2;
+}
+
+TEST(TestName, CombinationTest2) {
+  string cmd1 = "ls";
+  string cmd2 = "ls -a";
+  string cmd3 = "ls -l";
+  string cmd4 = "echo hello";
+  string cmd5 = "echo goodbye";
+
+  Base *test = new Command(cmd1);
+  Base *test0 = new Command(cmd2);
+  Base *test1 = new And_Connector(test, test0);
+  Base *test2 = new Command(cmd3);
+  Base *test3 = new And_Connector(test2, test1);
+
+  EXPECT_EQ(test3->getCommand(), "ls -l && ls && ls -a");
 }
 
 int main(int argc, char **argv) {
