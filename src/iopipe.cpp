22196fb68198abd2756302ba8c33f7fc845f8ec8
Jason Chang <jchan149@ucr.edu>
2019-06-09 16:00:49 -0700

    .

--- src/iopipe.cpp
+++ src/iopipe.cpp
@@ -22,97 +22,97 @@ class Base;
 class Connector;
 
 bool IOPipe::execute() {
-    int file_desc[2];
-    int temp;
-    pid_t pid1;
-    pid_t pid2;
-    int ret = pipe(file_desc);
- if (ret < 0) {  //error
-   perror("pipe");
-   exit(1);
-   return false;
- }
-
-    pid1 = fork();
-
-    if (pid1 < 0) {
-      perror("fork");
-      exit(1);
-      return false;
-    }
-
-    if (pid1 == 0) {
-      pid2 = fork();
-
-      if (pid2 < 0) {
-        perror("fork");
-        exit(1);
-        return false;
-      }
-      else if (pid2 == 0) {
-        if (dup2(file_desc[1], 1) < 0) {
-          perror("dup2");
-          exit(1);
-          return false;
-        }
-        if (close(file_desc[0]) < 0) {
-          perror("errno");
-          exit(1);
-          return false;
-        }
-        if (!left->execute()) {
-          exit(1);
-          return false;
-        }
-        exit(0);
-      }
-      else {
-        if (dup2(file_desc[0], 0) < 0) {
-          perror("dup2");
-          exit(1);
-          return false;
-        }
-        if (close(file_desc[1]) < 0) {
-          perror("errno");
-          exit(1);
-          return false;
-        }
-        if (!right->execute()) {
-          exit(1);
-          return false;
-        }
-        exit(0);
-      }
-    }
-
-    if (close(file_desc[0]) < 0) {
-      perror("errno");
-      exit(1);
-      return false;
-    }
-
-    if (close(file_desc[1]) < 0) {
-      perror("errno");
-      exit(1);
-      return false;
-    }
-
-    waitpid(pid1, &temp, 0);
-
-    while (!WIFEXITED(temp)) {
-    }
-    waitpid(pid1, &temp, 0);
-    if (temp > 0) {
-      return false;
-    }
-    else if (WEXITSTATUS(temp) == 0) {
-      return true;
-    }
-    else if (WEXITSTATUS(temp) == 1) {
-      return false;
-    }
-    return false;
-}
+//     int file_desc[2];
+//     int temp;
+//     pid_t pid1;
+//     pid_t pid2;
+//     int ret = pipe(file_desc);
+//  if (ret < 0) {  //error
+//    perror("pipe");
+//    exit(1);
+//    return false;
+//  }
+//
+//     pid1 = fork();
+//
+//     if (pid1 < 0) {
+//       perror("fork");
+//       exit(1);
+//       return false;
+//     }
+//
+//     if (pid1 == 0) {
+//       pid2 = fork();
+//
+//       if (pid2 < 0) {
+//         perror("fork");
+//         exit(1);
+//         return false;
+//       }
+//       else if (pid2 == 0) {
+//         if (dup2(file_desc[1], 1) < 0) {
+//           perror("dup2");
+//           exit(1);
+//           return false;
+//         }
+//         if (close(file_desc[0]) < 0) {
+//           perror("errno");
+//           exit(1);
+//           return false;
+//         }
+//         if (!left->execute()) {
+//           exit(1);
+//           return false;
+//         }
+//         exit(0);
+//       }
+//       else {
+//         if (dup2(file_desc[0], 0) < 0) {
+//           perror("dup2");
+//           exit(1);
+//           return false;
+//         }
+//         if (close(file_desc[1]) < 0) {
+//           perror("errno");
+//           exit(1);
+//           return false;
+//         }
+//         if (!right->execute()) {
+//           exit(1);
+//           return false;
+//         }
+//         exit(0);
+//       }
+//     }
+//
+//     if (close(file_desc[0]) < 0) {
+//       perror("errno");
+//       exit(1);
+//       return false;
+//     }
+//
+//     if (close(file_desc[1]) < 0) {
+//       perror("errno");
+//       exit(1);
+//       return false;
+//     }
+//
+//     waitpid(pid1, &temp, 0);
+//
+//     while (!WIFEXITED(temp)) {
+//     }
+//     waitpid(pid1, &temp, 0);
+//     if (temp > 0) {
+//       return false;
+//     }
+//     else if (WEXITSTATUS(temp) == 0) {
+//       return true;
+//     }
+//     else if (WEXITSTATUS(temp) == 1) {
+//       return false;
+//     }
+//     return false;
+// }
 
 string IOPipe::getCommand() {
   return left->getCommand() + " | " + right->getCommand();
