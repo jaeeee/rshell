2bfb5507bd03f5e3d73beb9a196fed02fcb63aee
Jason Chang <jchan149@ucr.edu>
2019-06-09 22:46:13 -0700

    added some tests but idk if work

--- unit_tests/unit_test.cpp
+++ unit_tests/unit_test.cpp
@@ -10,6 +10,7 @@
 #include "../header/base.h"
 #include "../header/command.h"
 #include "../header/iopipe.h"
+#include "../header/input.h"
 #include <iostream>
 #include <string>
 #include <cmath>
@@ -19,33 +20,57 @@ using namespace std;
 TEST(CountVisitorTest, IOPipeTest1) {
 	Base* cmd1 = new Command("ls");
 	Base* cmd2 = new Command("more");
-	Base* pip = new IOPipe(cmd1,cmd2);
+	Base* connector = new IOPipe(cmd1,cmd2);
 
-	EXPECT_TRUE(pip->execute());
+	EXPECT_TRUE(connector->execute());
 }
 
 TEST(CountVisitorTest, IOPipeTest2) {
 	Base* cmd1 = new Command("ls");
 	Base* cmd2 = new Command("less");
-	Base* pip = new IOPipe(cmd1,cmd2);
+	Base* connector = new IOPipe(cmd1,cmd2);
 
-	EXPECT_TRUE(pip->execute());
+	EXPECT_TRUE(connector->execute());
 }
 
 TEST(CountVisitorTest, IOPipeTest3) {
 	Base* cmd1 = new Command("ls");
 	Base* cmd2 = new Command("ls");
-	Base* pip = new IOPipe(cmd1,cmd2);
+	Base* connector = new IOPipe(cmd1,cmd2);
 
-	EXPECT_FALSE(pip->execute());
+	EXPECT_FALSE(connector->execute());
 }
 
 TEST(CountVisitorTest, IOPipeTest4) {
 	Base* cmd1 = new Command("ls");
 	Base* cmd2 = new Command("");
-	Base* pip = new IOPipe(cmd1,cmd2);
+	Base* connector = new IOPipe(cmd1,cmd2);
 
-	EXPECT_FALSE(pip->execute());
+	EXPECT_FALSE(connector->execute());
+}
+
+TEST(CountVisitorTest, ABracketTest1) {
+	Base* cmd1 = new Command("wc");
+	Base* cmd2 = new Command("names.txt");
+	Base* connector = new Input(cmd1,cmd2);
+
+	EXPECT_TRUE(connector->execute());
+}
+
+TEST(CountVisitorTest, ABracketTest2) {
+	Base* cmd1 = new Command("wc");
+	Base* cmd2 = new Command("asdftxt");
+	Base* connector = new Input(cmd1,cmd2);
+
+	EXPECT_FALSE(connector->execute());
+}
+
+TEST(CountVisitorTest, ABracketTest3) {
+	Base* cmd1 = new Command("cat names.txt");
+	Base* cmd2 = new Command("names2.txt");
+	Base* connector = new Input(cmd1,cmd2);
+
+	EXPECT_FALSE(connector->execute());
 }
 
 int main(int argc, char **argv) {
