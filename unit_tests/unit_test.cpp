af5772605c39d0736a6ace29403b42513ecfdade
Jason Chang <jchan149@ucr.edu>
2019-06-09 21:06:25 -0700

    added some unit test

--- unit_tests/unit_test.cpp
+++ unit_tests/unit_test.cpp
@@ -9,11 +9,44 @@
 #include "../header/semi.h"
 #include "../header/base.h"
 #include "../header/command.h"
+#include "../header/iopipe.h"
 #include <iostream>
 #include <string>
 #include <cmath>
 
 using namespace std;
+
+TEST(CountVisitorTest, IOPipeTest1) {
+	Base* cmd1 = new Command("ls");
+	Base* cmd2 = new Command("more");
+	Base* pip = new IOPipe(cmd1,cmd2);
+
+	EXPECT_TRUE(pip->execute());
+}
+
+TEST(CountVisitorTest, IOPipeTest2) {
+	Base* cmd1 = new Command("ls");
+	Base* cmd2 = new Command("less");
+	Base* pip = new IOPipe(cmd1,cmd2);
+
+	EXPECT_TRUE(pip->execute());
+}
+
+TEST(CountVisitorTest, IOPipeTest3) {
+	Base* cmd1 = new Command("ls");
+	Base* cmd2 = new Command("ls");
+	Base* pip = new IOPipe(cmd1,cmd2);
+
+	EXPECT_FALSE(pip->execute());
+}
+
+TEST(CountVisitorTest, IOPipeTest4) {
+	Base* cmd1 = new Command("ls");
+	Base* cmd2 = new Command("");
+	Base* pip = new IOPipe(cmd1,cmd2);
+
+	EXPECT_FALSE(pip->execute());
+}
 
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
